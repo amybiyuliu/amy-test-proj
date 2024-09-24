0f3f64216578d3d8c3b51d7298327a49573c4080
Amy Liu <amy.biyu.liu@gmail.com>
2024-09-24 15:20:41 +0930

    first commit

---
+++ spec/basic_test1_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe do 
+  it 'returns true' do 
+    expect(true).to eq(true)
+  end
+
+  it 'returns false' do 
+    expect(false).to eq(false)
+  end 
+end 
---
+++ spec/basic_test2_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe do 
+  it 'returns banana' do 
+    expect("banana").to eq("banana")
+  end
+
+  it 'returns apple' do 
+    expect("apple").to eq("apple")
+  end 
+end
